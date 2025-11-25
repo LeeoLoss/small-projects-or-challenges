9a9e677b39f7959ec67ceb5639bf59030b9d86aa
Leonardo Loss <132675221+LeeoLoss@users.noreply.github.com>
2025-11-25 13:22:39 -0300

    Add donation-register.cs with donation details

---
+++ lista-exercicios.cs/donation-register.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        float valorRecebido = 500.00f;
+        bool doacaoAnonima = true;
+        char tipoConta = 'P';
+
+        Console.WriteLine("Valor Recebido: " + valorRecebido);
+        Console.WriteLine("Doação Anônima: " + doacaoAnonima);
+        Console.WriteLine("Tipo de Conta: " + tipoConta);
+    }
+}
